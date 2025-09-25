5ff17acfffda406082324f2a8ede118c6db685f6
keval Jadhav <kevaljadhav87@gmail.com>
2025-09-25 18:07:35 +0000

    here is the string and its commands

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,7 +9,46 @@ console.log(`hello My name is ${name} and my repo count is ${repoCount}`);
 const gamename = new String('keval jadhav')
 
 
-console.log(gamename[0]);
-console.log(gamename.__proto__);
+// console.log(gamename[0]);
+
+
+// console.log(gamename.__proto__);
+
+
+// console.log(gamename.length);//tell us length of the string
+
+
+// console.log(gamename.toUpperCase());// convert lowercase words into upercase
+
+
+// console.log(gamename.charAt(6));// by using this we can fine string characther by give its place no
+
+
+// console.log(gamename.indexOf('j')); //by using this we can fine defined string charachter no place
+
+
+
+const newString = gamename.substring(0,3)// it is 
+
+
+
+const anoternewString = gamename.slice(-2,1)
+
+console.log(newString);
+
+
+const onemorestring = "     keval    "
+
+console.log(onemorestring);
+console.log(onemorestring.trim());// it can remove the space in the entered value
+
+
+const url = "https://kevaljadhav.com/20%home/page"
+
+
+console.log(url.replace('20%','-'));
+
+console.log(url.includes('keval'));
+console.log(url.includes('anyone'));
 
 
